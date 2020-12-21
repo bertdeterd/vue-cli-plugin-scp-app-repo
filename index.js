35915555696f29bdf01bdda6cada37a4202429e7
860430 <bert.deterd2@stedin.net>
2020-12-21 09:22:08 +0100

    init

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = (api, projectOptions) => {
       // modify webpack config
       // or return object to be merged with webpack-merge
     })
-  
+  //
     api.registerCommand('test', args => {
       // register `vue-cli-service test`
     })
